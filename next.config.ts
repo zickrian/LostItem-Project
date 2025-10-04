32812e700ba5874da6de427c6df8c2a038327cd3
FirdausKz <espejodaniel50@gmail.com>
2025-10-04 19:56:50 +0700

    feat: Add remote pattern for Supabase storage in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,11 @@ const nextConfig: NextConfig = {
         hostname: 'lh3.googleusercontent.com',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'oxjfahzrzjdukwksmcem.supabase.co',
+        pathname: '/storage/v1/object/public/**',
+      },
     ],
   },
 };
